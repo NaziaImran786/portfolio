8f936838a772792021a302b5f8a1c377af83e911
NaziaImran786 <naziaimran.4df@gmail.com>
2024-09-29 19:51:50 +0500

    first commit

---
+++ milestone1-static-resume/script.js
@@ -0,0 +1,8 @@
+"use strict";
+var _a;
+(_a = document.getElementById('toggle-skills')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    const skillsSection = document.getElementById('skills');
+    if (skillsSection) {
+        skillsSection.style.display = (skillsSection.style.display === 'none') ? 'block' : 'none';
+    }
+});
